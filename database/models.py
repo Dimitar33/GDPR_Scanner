8da04254a81fc7cdd546a929ddfc9d0fb02a6421
Dimitar33 <dimpetkov777@gmail.com>
2026-01-26 22:21:18 +0000

    db build

---
+++ database/models.py
@@ -0,0 +1,21 @@
+
+from .db import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
+
+class Scan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    scan_date = db.Column(db.DateTime, nullable=False)
+    result = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"Scan('{self.user_id}', '{self.scan_date}')"
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-from flask import Flask
+from flask import Flask , render_template
+from flask_sqlalchemy import SQLAlchemy
+from database.db import db
+
 
 app = Flask(__name__)
 
+## Create Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+from database.models import User, Scan
+## Create the tables
+with app.app_context():
+    db.create_all()
+
+## Routes
 @app.route('/')
 
 def hello():
-    return "Hello, GDPR scanner!"
+    return render_template('index.html')
+
+@app.route('/scan')
+def scan():
+    return render_template('scan.html')
 
 
-@app.route('/bye/<name>')
-def buy(name):
-    return f"bye, {name}!"
-
+## Run the App
 if __name__ == '__main__':
     app.run(debug=True)
